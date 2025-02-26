53d294f92198aa3457923bf0a7df157d689b74c1
Eben <me@ebenroux.co.za>
2025-02-26 17:24:19 +0200

    Fixed recursive bug.

--- Shuttle.Access/DataAccess/DataStoreAccessService.cs
+++ Shuttle.Access/DataAccess/DataStoreAccessService.cs
@@ -66,7 +66,7 @@ public class DataStoreAccessService : CachedAccessService, IAccessService
 
     private async Task CacheAsync(Guid token, CancellationToken cancellationToken)
     {
-        if (await ContainsAsync(token, cancellationToken))
+        if (await base.ContainsAsync(token))
         {
             return;
         }
