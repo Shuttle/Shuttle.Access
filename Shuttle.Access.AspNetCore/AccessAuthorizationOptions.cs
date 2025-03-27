57e6e507b11137b1473d29846975386f16fda431
Eben <me@ebenroux.co.za>
2025-03-27 08:28:28 +0200

    Authorization options.

---
+++ Shuttle.Access.AspNetCore/AccessAuthorizationOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace Shuttle.Access.AspNetCore;
+
+public class AccessAuthorizationOptions
+{
+    public const string SectionName = "Shuttle:Access:Authorization";
+    public List<IssuerOptions> Issuers { get; set; } = [];
+}
---
+++ Shuttle.Access.AspNetCore/IssuerOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Shuttle.Access.AspNetCore;
+
+public class IssuerOptions
+{
+    public List<string> Audiences { get; set; } = [];
+    public TimeSpan ClockSkew { get; set; } = TimeSpan.FromMinutes(5);
+    public List<string> IdentityNameClaimTypes { get; set; } = [];
+    public string JwksUri { get; set; } = string.Empty; // JSON Web Key Store
+    public TimeSpan SigningKeyCacheDuration { get; set; } = TimeSpan.FromHours(1);
+    public string Uri { get; set; } = string.Empty;
+}
