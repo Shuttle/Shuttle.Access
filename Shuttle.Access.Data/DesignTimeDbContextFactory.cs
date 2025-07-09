cdb3e4252b6aee576434e27b068c97b6f8816216
Eben <me@ebenroux.co.za>
2025-07-09 18:38:18 +0200

    Data.

--- Shuttle.Access.Data/DesignTimeDbContextFactory.cs
+++ Shuttle.Access.Data/DesignTimeDbContextFactory.cs
@@ -8,14 +8,22 @@ public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AccessDbCo
 {
     public AccessDbContext CreateDbContext(string[] args)
     {
+        /*
+            Right-click on `Shuttle.Access.Data` and select `Manage User Secrets`
+            {
+              "ConnectionStrings": {
+                "Access": "Data Source=.;Initial Catalog=Access;user id=<user>;password=<password>;TrustServerCertificate=true"
+              }
+            }
+        */
+
         var configuration = new ConfigurationBuilder()
             .AddUserSecrets<DesignTimeDbContextFactory>()
             .Build();
 
         var optionsBuilder = new DbContextOptionsBuilder<AccessDbContext>();
         
-        optionsBuilder.UseSqlServer(configuration.GetConnectionString("Access"),
-            builder => builder.MigrationsHistoryTable("__EFMigrationsHistory"));
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("Access"));
 
         return new(optionsBuilder.Options);
     }
